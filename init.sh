7804d5cf26e73dcd6b8966f310561b46f831585c
Lingqiang He <he614@purdue.edu>
2023-06-19 11:50:55 -0400

    the Ntuplizer/Delphes_Ntuplizer_custom.py needs the default fcc env to run

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 module load anaconda/2020.11-py38
 source activate mycmsenv
-#source /cvmfs/fcc.cern.ch/sw/latest/setup.sh
+source /cvmfs/fcc.cern.ch/sw/latest/setup.sh
 conda activate /depot/cms/conda_envs/jthiema/Coffea-Jason
 export HLLHC_BASE=$PWD
