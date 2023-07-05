18b0c63deac5e7ef85da64fa4e040abd82931f96
Lingqiang He <he614@purdue.edu>
2023-07-05 11:07:08 -0400

    changed file paths on Make_Minitrees_Commands.py

--- Make_Minitrees_Commands.py
+++ Make_Minitrees_Commands.py
@@ -14,11 +14,10 @@ if not os.path.exists("MiniTreeOutput"): os.makedirs("MiniTreeOutput")
 for line in lines :
     
     line = line.strip('\n')
-    outputfilestring = "minitree_" +line.replace("/mnt/hadoop/store/user/hyeonseo/mgp8_pp_tt012j_5f/events_","")
+    outputfilestring = "minitree_" +line.replace("/eos/purdue/store/user/abakshi/TTBar_Ntuples/TT_Dilept_","")
     commandfile.write("python Minitrees/Make_minitrees.py -i "+line+" -o MiniTreeOutput/"+ outputfilestring +"\n")
     commandfile.write("python Top_reco/Top_reco.py -i MiniTreeOutput/"+ outputfilestring+"\n")
     
     
 f.close()
 commandfile.close()
-
